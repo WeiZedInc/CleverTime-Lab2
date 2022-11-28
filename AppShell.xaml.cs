95c126b7d4ef17bf5566430a66f52d19c474c879
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2022-11-28 22:21:15 +0200

    Добавьте файлы проекта.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CleverTime;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CleverTime;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
