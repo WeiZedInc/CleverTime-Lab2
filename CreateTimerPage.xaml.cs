fdddd6a2d5c94c7afaa8a875f9c69ad28a4f91e1
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2022-11-29 11:40:20 +0200

    sliders added

--- CreateTimerPage.xaml.cs
+++ CreateTimerPage.xaml.cs
@@ -1,15 +1,92 @@
+using Microsoft.Maui;
+
 namespace CleverTime;
 
 public partial class CreateTimerPage : ContentPage
 {
-	public CreateTimerPage()
+    Label hoursLabel, minutesLabel, secondsLabel;
+    Slider hoursSlider, minutesSlider, secondsSlider;
+    Grid childGrid;
+
+    public CreateTimerPage()
 	{
 		InitializeComponent();
-	}
+    }
 
-
-	private void NameInput_Completed(object sender, EventArgs e)
+    private void NameInput_Completed(object sender, EventArgs e)
 	{
         string text = ((Entry)sender).Text;
+    }
+
+    private void isFromNowCheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+	{
+		if (e.Value == false)
+		{
+            CreateTimeSliders();
+            RootLayout.Add(childGrid);
+        }
+		else
+            RootLayout.Remove(childGrid);
+	}
+    void OnHoursSliderChanged(object sender, ValueChangedEventArgs e)
+    {
+        hoursLabel.Text = "Hours: " + ((int)hoursSlider.Value).ToString();
+    }
+    void OnMinutesSliderChanged(object sender, ValueChangedEventArgs e)
+    {
+        minutesLabel.Text = "Minutes: " + ((int)minutesSlider.Value).ToString();
+    }
+    void OnSecondsSliderChanged(object sender, ValueChangedEventArgs e)
+    {
+        secondsLabel.Text = "Seconds: " + ((int)secondsSlider.Value).ToString();
+    }
+    void CreateTimeSliders()
+    {
+        childGrid = new Grid
+        {
+            Margin = new Thickness(20),
+            RowDefinitions =
+            {
+                new RowDefinition(),
+                new RowDefinition(),
+                new RowDefinition(),
+                new RowDefinition(),
+                new RowDefinition(),
+                new RowDefinition()
+            }
+        };
+
+        hoursSlider = new Slider();
+        hoursSlider.ValueChanged += OnHoursSliderChanged;
+        hoursSlider.Minimum = 0;
+        hoursSlider.Maximum = 24;
+        childGrid.Add(hoursSlider);
+
+        minutesSlider = new Slider();
+        minutesSlider.ValueChanged += OnMinutesSliderChanged;
+        minutesSlider.Minimum = 0;
+        minutesSlider.Maximum = 60;
+        Grid.SetRow(minutesSlider, 2);
+        childGrid.Add(minutesSlider);
+
+        secondsSlider = new Slider();
+        secondsSlider.ValueChanged += OnSecondsSliderChanged;
+        secondsSlider.Minimum = 0;
+        secondsSlider.Maximum = 60;
+        Grid.SetRow(secondsSlider, 4);
+        childGrid.Add(secondsSlider);
+
+
+        hoursLabel = new Label();
+        Grid.SetRow(hoursLabel, 1);
+        childGrid.Add(hoursLabel);
+
+        minutesLabel = new Label();
+        Grid.SetRow(minutesLabel, 3);
+        childGrid.Add(minutesLabel);
+
+        secondsLabel = new Label();
+        Grid.SetRow(secondsLabel, 5);
+        childGrid.Add(secondsLabel);
     }
 }
